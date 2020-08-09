ecd97661b607aad0fb964c44c12b45d55c68bbc0
Gustavo Lima <gustavosnl@gmail.com>
2020-08-09 16:54:28 -0300

    adequando a interface de serviço para integração com Flow

--- data/src/main/java/com/glima/data/service/RepositoryService.kt
+++ data/src/main/java/com/glima/data/service/RepositoryService.kt
@@ -1,16 +1,16 @@
 package com.glima.data.service
 
 import com.glima.data.dto.RepositoriesSearchResultResponse
-import kotlinx.coroutines.Deferred
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface RepositoryService {
 
     @GET("search/repositories?")
-    fun fetchRepositoriesByQuery(
+    suspend fun fetchRepositoriesByQuery(
         @Query("q") searchTerm: String,
         @Query("sortType") sortType: String,
-        @Query("page") pageNumber: Int
-    ): Deferred<RepositoriesSearchResultResponse>
+        @Query("page") pageNumber: Int,
+        @Query("per_page") itemsPerPage: Int
+    ): RepositoriesSearchResultResponse
 }
