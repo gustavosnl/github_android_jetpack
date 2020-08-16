d4f0f1d41adda8dc6d06fb58a89898fcee71d0a0
Gustavo Lima <gustavosnl@gmail.com>
2020-08-16 00:43:12 -0300

    fix do loading de imagens, url pode ser nula

--- app/src/main/java/com/glima/githubrepolist/BindingAdapters.kt
+++ app/src/main/java/com/glima/githubrepolist/BindingAdapters.kt
@@ -5,7 +5,7 @@ import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 
 @BindingAdapter("repositoryImgUrl")
-fun repositoryImgUrl(imageView: ImageView, url: String ) {
+fun repositoryImgUrl(imageView: ImageView, url: String? ) {
     Glide.with(imageView.context)
         .load(url)
 //        .apply(RequestOptions()
