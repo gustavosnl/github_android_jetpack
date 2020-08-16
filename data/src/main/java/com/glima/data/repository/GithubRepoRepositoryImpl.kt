8ea3e1c83fa5eececa9b9b468a40dcef090a60ba
Gustavo Lima <gustavosnl@gmail.com>
2020-08-16 00:40:57 -0300

    substituindo o paging source pelo remote mediator no repositório

--- data/src/main/java/com/glima/data/repository/GithubRepoRepositoryImpl.kt
+++ data/src/main/java/com/glima/data/repository/GithubRepoRepositoryImpl.kt
@@ -1,55 +1,31 @@
 package com.glima.data.repository
 
-import androidx.paging.Pager
-import androidx.paging.PagingConfig
-import androidx.paging.PagingData
-import androidx.paging.PagingSource
+import androidx.paging.*
+import com.glima.data.database.GithubRepositoryDatabase
 import com.glima.data.mapper.asDomain
 import com.glima.data.service.RepositoryService
 import com.glima.domain.business.model.Repository
 import com.glima.domain.repository.GithubRepoRepository
 import com.glima.domain.repository.SearchParams
 import kotlinx.coroutines.flow.Flow
-import timber.log.Timber
+import kotlinx.coroutines.flow.map
 
-class GithubRepoRepositoryImpl(private val service: RepositoryService) :
-    GithubRepoRepository {
+class GithubRepoRepositoryImpl(
+    private val service: RepositoryService,
+    private val database: GithubRepositoryDatabase
+) : GithubRepoRepository {
+
+    @ExperimentalPagingApi
     override suspend fun searchRepositoriesByQuery(query: SearchParams): Flow<PagingData<Repository>> {
         return Pager(
-            config = PagingConfig(pageSize = 20),
-            pagingSourceFactory = { RepositoryPagingSource(service, query) }
-        ).flow
-    }
-}
-
-private const val STARTING_PAGE = 1
-
-class RepositoryPagingSource(
-    private val service: RepositoryService,
-    private val searchParams: SearchParams
-) : PagingSource<Int, Repository>() {
-    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Repository> {
-        val position = params.key ?: STARTING_PAGE
-        Timber.d("position: $position")
-        return try {
-            val result =
-                service.fetchRepositoriesByQuery(
-                    searchParams.query,
-                    searchParams.sort.sort,
-                    position,
-                    params.loadSize
-                )
-
-            LoadResult.Page(
-                data = result.asDomain().repositories,
-                prevKey = if (position == STARTING_PAGE) null else position - 1,
-                nextKey = if (result.repositories.isEmpty()) null else position + 1
-            )
-
-        } catch (e: Exception) {
-            Timber.e("error: ${e.message}")
-            LoadResult.Error(e)
+            config = PagingConfig(pageSize = 50),
+            remoteMediator = RepositoryRemoteMediator(query, database, service),
+            pagingSourceFactory = { database.repositoryDAO.getAll() }
+        ).flow.map {
+            it.map { repo ->
+                repo.asDomain()
+            }
         }
+
     }
 }
-
