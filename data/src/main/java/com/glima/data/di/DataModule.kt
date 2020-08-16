6e5dc39706f2cf37768bf15dd70bf8394f5376a3
Gustavo Lima <gustavosnl@gmail.com>
2020-08-16 00:34:00 -0300

    provendo Database através do KOIN

--- data/src/main/java/com/glima/data/di/DataModule.kt
+++ data/src/main/java/com/glima/data/di/DataModule.kt
@@ -1,8 +1,8 @@
 package com.glima.data.di
 
 import androidx.room.Room
-import com.glima.data.GithubRepositoryDatabase
-import com.glima.data.dao.RepositoryDAO
+import com.glima.data.database.GithubRepositoryDatabase
+import com.glima.data.database.RepositoryDAO
 import com.glima.data.repository.GithubRepoRepositoryImpl
 import com.glima.domain.repository.GithubRepoRepository
 import org.koin.android.ext.koin.androidContext
@@ -27,7 +27,10 @@ object DataModule {
         }
 
         single<GithubRepoRepository> {
-            GithubRepoRepositoryImpl(get())
+            GithubRepoRepositoryImpl(
+                service = get(),
+                database = get()
+            )
         }
     }
 }
